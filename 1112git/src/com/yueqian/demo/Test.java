caffa34c5fd8bf6ff698f74eca591187b34e5ae3
laihongqu <2253865616@qq.com>
2019-11-12 10:47:10 +0800

    idea第二次代码

--- 1112git/src/com/yueqian/demo/Test.java
+++ 1112git/src/com/yueqian/demo/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("idea测试");
         System.out.println("diea第二次");
+        System.out.println("第三次");
     }
 }
